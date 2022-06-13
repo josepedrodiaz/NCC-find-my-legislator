17121491a740d9bea71e422173a12f0ed5655d64
Pedro D <josepedrodiaz@gmail.com>
2022-06-13 16:44:59 -0300

    Add API and methods to recover information from API

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 console.log('IS WORKING');
+var API_KEY = "AIzaSyAJzRTiaQvUM6wvXV-rXYJJUMoh8czJgws";
+/**
+   * Sample JavaScript code for civicinfo.representatives.representativeInfoByAddress
+   * See instructions for running APIs Explorer code samples locally:
+   * https://developers.google.com/explorer-help/code-samples#javascript
+   */
+
+ function loadClient() {
+  gapi.client.setApiKey(API_KEY);
+  return gapi.client.load("https://civicinfo.googleapis.com/$discovery/rest?version=v2")
+      .then(function() { console.log("GAPI client loaded for API"); },
+            function(err) { console.error("Error loading GAPI client for API", err); });
+}
+// Make sure the client is loaded before calling this method.
+function execute() {
+  return gapi.client.civicinfo.representatives.representativeInfoByAddress({
+    "address": "279 Huff Dr, Jacksonville, NC 28546, Estados Unidos"
+  })
+      .then(function(response) {
+              // Handle the results here (response.result has the parsed body).
+              console.log("Response", response);
+            },
+            function(err) { console.error("Execute error", err); });
+}
+gapi.load("client");
